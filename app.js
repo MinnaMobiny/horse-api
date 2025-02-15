28db78a3ae7dd0eff459eba516c4bdddcab799ff
MinnaMobiny <minnamobiny@hotmail.com>
2025-02-15 19:38:27 +0100

    Lade till CORS

--- app.js
+++ app.js
@@ -45,7 +45,7 @@ mongoose
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
-  .then(() => console.log("✅ MongoDB connected"))
-  .catch((err) => console.error("❌ MongoDB connection error:", err));
+  .then(() => console.log("MongoDB connected"))
+  .catch((err) => console.error("MongoDB connection error:", err));
 
 module.exports = app;
